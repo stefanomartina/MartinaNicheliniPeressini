ff093efd049392cc554ec9344c572192120f4b22
Stefano <stefano.martina@mail.polimi.it>
2018-11-28 11:32:05 +0100

    Added check user and add user

--- Server/DbHandler.py
+++ Server/DbHandler.py
@@ -16,12 +16,35 @@ class DBHandler:
         self.dbMy.execute(query)
 
         for x in self.dbMy:
-            print(x[1])
-
-            if x[1] == psw :
+            if x[1] == psw:
                 return True
-            else :
+            else:
                 return False
+
+
+    def create_usr(self, usr, psw, fname, lname, birthday):
+        query = "INSERT INTO User VALUES ('"+usr+"','"+ psw+"','"+lname+"','"+fname+"','"+ birthday+"')";
+        print(query)
+
+
+    def check_usr(self, username):
+        query = "SELECT password FROM User WHERE username = \""+username+"\"";
+
+        print(query)
+
+        self.dbMy.execute(query)
+
+        for x in self.dbMy:
+            if x[0] != None:
+                return x[0]
+            else:
+                return None
+
+
+d = DBHandler()
+#d.create_usr("pippo", "pippopsw",  "pippopsw", "pippopsw","1996-07-02")
+
+print(d.check_usr("mario"))
 
 
 
