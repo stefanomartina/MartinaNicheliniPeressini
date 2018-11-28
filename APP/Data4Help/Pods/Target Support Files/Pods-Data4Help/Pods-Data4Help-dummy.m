a16da383cd5c0985f6918a8c9b3eae251291345f
Francesco Peressini <francesco.peressini@hotmail.com>
2018-11-28 13:52:43 +0100

    New ui implementation

--- APP/Data4Help/Pods/Target Support Files/Pods-Data4Help/Pods-Data4Help-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Data4Help : NSObject
-@end
-@implementation PodsDummy_Pods_Data4Help
-@end
---
+++ data4help/data4help/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  data4help
+//
+//  Created by Francesco Peressini on 28/11/2018.
+//  Copyright © 2018 Francesco Peressini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
