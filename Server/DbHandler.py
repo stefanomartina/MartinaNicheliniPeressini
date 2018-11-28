446397c3e76a61c6a84975bf3eaa3ab6041bf34a
Stefano <stefano.martina@mail.polimi.it>
2018-11-28 10:54:37 +0100

    Created DbHandler

---
+++ Server/DbHandler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import mysql.connector
+
+class DBHandler:
+
+    def __init__(self):
+        self.db = mysql.connector.connect( host = '35.198.157.139', database='data4help', user='root', passwd='trackme');
+        self.dbMy = self.db.cursor()
+
+    def auth(self, usr, psw):
+        query = "SELECT * FROM User WHERE username = \"" + usr +"\" ";
+
+        print(query)
+
+        self.dbMy.execute(query)
+
+        for x in self.dbMy:
+            print(x[1])
+
+            if x[1] == psw :
+                print("OK")
+            else :
+                print("NO");
+
+
+
