db48ffbf7888fc77df323448a879f5f45722e053
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-11-28 11:07:45 +0100

    PEP8 compliant

--- Server/DbHandler.py
+++ Server/DbHandler.py
@@ -2,10 +2,11 @@
 
 import mysql.connector
 
+
 class DBHandler:
 
     def __init__(self):
-        self.db = mysql.connector.connect( host = '35.198.157.139', database='data4help', user='root', passwd='trackme');
+        self.db = mysql.connector.connect(host='35.198.157.139', database='data4help', user='root', passwd='trackme')
         self.dbMy = self.db.cursor()
 
     def auth(self, usr, psw):
@@ -18,9 +19,9 @@ class DBHandler:
         for x in self.dbMy:
             print(x[1])
 
-            if x[1] == psw :
+            if x[1] == psw:
                 return True
-            else :
+            else:
                 return False
 
 
