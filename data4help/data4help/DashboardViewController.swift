988edf62f78d102e81c0a4cfcb01c9a61cb31893
Stefano <stefano.martina@mail.polimi.it>
2018-11-30 17:51:06 +0100

    new view

---
+++ data4help/data4help/DashboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardViewController.swift
+//  data4help
+//
+//  Created by Stefano Martina on 30/11/2018.
+//  Copyright © 2018 Francesco Peressini. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
