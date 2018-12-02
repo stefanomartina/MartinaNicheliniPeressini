4282d3bf184dfd972a209c5253b54baf82c69527
Francesco Peressini <francesco.peressini@hotmail.com>
2018-12-02 23:24:55 +0100

    Login correctly working

--- data4help/data4help/LoginViewController.swift
+++ data4help/data4help/LoginViewController.swift
@@ -1,5 +1,5 @@
-import UIKit
 import Alamofire
+import UIKit
 
 class LoginViewController: UIViewController {
     
@@ -10,7 +10,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBAction func loginButton(_ sender: UIButton){
+    @IBAction func loginButton(_ sender: UIButton) {
         username = usernameTextField.text!
         password = passwordTextField.text!
         let credential = URLCredential(user: username, password: password, persistence: .forSession)
@@ -20,26 +20,22 @@ class LoginViewController: UIViewController {
             .responseJSON {
                 response in
                 
-                if let status = response.result.value{
+                if let status = response.result.value {
                     let JSON = status as! NSDictionary
                     
-                    if let result = JSON["Response"] as? String{
-                        if(result == "-1"){
-                            if let message = JSON["Message"] as? String{
+                    if let result = JSON["Response"] as? String {
+                        if(result == "-1") {
+                            if let message = JSON["Message"] as? String {
                                 let alert = UIAlertController(title: "Attention", message: message, preferredStyle: .alert)
                                 alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
                                 self.present(alert, animated: true)
                             }
-                        }else{
-                            self.performSegue(withIdentifier: "LoginToDashboard", sender: self)
                         }
-                    }else{
-                        print("non sono entrato")
+                        else {
+                            self.performSegue(withIdentifier: "loginToDashboard", sender: nil)
+                        }
                     }
-                    
                 }
-                
-                
         }
     }
     
@@ -49,22 +45,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        
-    }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
