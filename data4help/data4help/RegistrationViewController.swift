7d1e440a26e082e3ae393a951ec8342f4227cfef
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-12-11 11:21:32 +0100

    Added datePicker and aspect ratio contraints

--- data4help/data4help/RegistrationViewController.swift
+++ data4help/data4help/RegistrationViewController.swift
@@ -13,6 +13,7 @@ extension UIViewController {
     }
 }
 
+
 class RegistrationViewController: UIViewController {
     
     let URL_USER_REGISTER = Global.getUserURL() + Global.REGISTER_METHOD
@@ -22,9 +23,10 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var textFieldLastName: UITextField!
     @IBOutlet weak var textFieldUsername: UITextField!
     @IBOutlet weak var textFieldPassword: UITextField!
-    
     @IBOutlet weak var textFieldBirthPlace: UITextField!
+    
     @IBOutlet weak var textFieldBirthDate: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
     
     @IBOutlet weak var textFieldGender: UITextField!
     
@@ -37,8 +39,13 @@ class RegistrationViewController: UIViewController {
             "firstname":textFieldFirstName.text!,
             "lastname":textFieldLastName.text!,
             "username":textFieldUsername.text!,
-            "password":textFieldPassword.text!
+            "password":textFieldPassword.text!,
+            "birthdate": textFieldBirthDate.text!,
+            "birthplace": textFieldBirthPlace.text!,
+            "gender": textFieldBirthPlace.text!,
+            "fiscalcode": textFieldFiscalCode.text!
         ]
+        
         Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters, encoding: JSONEncoding.default)
             .responseJSON {
                 response in
@@ -50,10 +57,26 @@ class RegistrationViewController: UIViewController {
         }
     }
 
+     /////////////////////////////////////////////////// DATE KEEPER
+    
+    @IBAction func dp(_ sender: UITextField) {
+        let datePickerView = UIDatePicker()
+        datePickerView.datePickerMode = .date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(handleDatePicker(sender:)), for: .valueChanged)
+    }
+    
+    @objc func handleDatePicker(sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMM yyyy"
+        textFieldBirthDate.text = dateFormatter.string(from: sender.date)
+    }
+    
+    
+    /////////////////////////////////////////////////// OVERRIDE DEFAULT VIEW FUNCTION
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        genderPicker.isHidden = true
         self.hideKeyboardWhenTappedAround()
     }
     
