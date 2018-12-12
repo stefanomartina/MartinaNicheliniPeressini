6da6123db5dc70662dc1ec8a0f2987f3a2fb39ff
Stefano <stefano.martina@mail.polimi.it>
2018-12-12 09:17:12 +0100

    Fixed errore

--- data4help/data4help/DashboardViewController.swift
+++ data4help/data4help/DashboardViewController.swift
@@ -78,20 +78,105 @@ class DashboardViewController: UIViewController {
     }
     
     @IBAction func updateAndUpload(_ sender: UIButton) {
-        var returned: [Double] = []
-        if returned.count == 0 {
-            let message = "No new data found"
-            let alert = UIAlertController(title: "Attention", message: message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alert, animated: true)
+        print("click")
+        subscribeToHeartBeatChanges()
+    }
+    
+    public func subscribeToHeartBeatChanges() {
+        
+        // Creating the sample for the heart rate
+        guard let sampleType: HKSampleType =
+            HKObjectType.quantityType(forIdentifier: .heartRate) else {
+                return
+        }
+        
+        /// Creating an observer, so updates are received whenever HealthKit’s
+        // heart rate data changes.
+        let heartRateQuery = HKObserverQuery.init(
+            sampleType: sampleType,
+            predicate: nil) { [weak self] _, _, error in
+                guard error == nil else {
+                    print("error!")
+                    return
+                }
+                
+                /// When the completion is called, an other query is executed
+                /// to fetch the latest heart rate
+                self?.fetchLatestHeartRateSample(completion: { sample in
+                    guard let sample = sample else {
+                        return
+                    }
+                    
+                    /// The completion in called on a background thread, but we
+                    /// need to update the UI on the main.
+                    DispatchQueue.main.async {
+                        
+                        /// Converting the heart rate to bpm
+                        let heartRateUnit = HKUnit(from: "count/min")
+                        let heartRate = sample
+                            .quantity
+                            .doubleValue(for: heartRateUnit)
+                        
+                        /// Updating the UI with the retrieved value
+                        print("\(Int(heartRate))")
+                    }
+                })
         }
     }
     
+<<<<<<< Updated upstream
+=======
+    public func fetchLatestHeartRateSample(
+        completion: @escaping (_ sample: HKQuantitySample?) -> Void) {
+        
+        /// Create sample type for the heart rate
+        guard let sampleType = HKObjectType.quantityType(forIdentifier: .heartRate) else {
+                completion(nil)
+                return
+        }
+        
+        print("fatto 1")
+        
+        /// Predicate for specifiying start and end dates for the query
+        let predicate = HKQuery
+            .predicateForSamples(
+                withStart: Date.distantPast,
+                end: Date(),
+                options: .strictEndDate)
+        
+        /// Set sorting by date.
+        let sortDescriptor = NSSortDescriptor(
+            key: HKSampleSortIdentifierStartDate,
+            ascending: false)
+        
+        /// Create the query
+        let query = HKSampleQuery(
+            sampleType: sampleType,
+            predicate: predicate,
+            limit: Int(HKObjectQueryNoLimit),
+            sortDescriptors: [sortDescriptor]) { (_, results, error) in
+                
+                guard error == nil else {
+                    print("Error: \(error!.localizedDescription)")
+                    return
+                }
+                
+                completion(results?[0] as? HKQuantitySample)
+        }
+        
+        healthStore.execute(query)
+    }
+    //@IBOutlet weak var totalSteps: UILabel!
+    
+>>>>>>> Stashed changes
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+<<<<<<< Updated upstream
     
     
     
+=======
+>>>>>>> Stashed changes
 }
