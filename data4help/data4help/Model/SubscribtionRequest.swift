e115c74005d231b9d766d594acde8d45094f4baf
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-12-19 18:49:59 +0100

    added again

---
+++ data4help/data4help/Model/SubscribtionRequest.swift
@@ -0,0 +1,9 @@
+//
+//  SubscribtionRequest.swift
+//  data4help
+//
+//  Created by Alessandro Nichelini on 19/12/2018.
+//  Copyright © 2018 Francesco Peressini. All rights reserved.
+//
+
+import Foundation
